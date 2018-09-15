ed4f5d2011431ce85876da859c86f833f27912b5
Ulas <ozulashe@gmail.com>
2018-09-15 14:58:47 +0300

    Visual

--- PassingParameters/PassingParameters/Program.cs
+++ PassingParameters/PassingParameters/Program.cs
@@ -24,13 +24,13 @@ namespace PassingParameters
         {
             Program obj = new Program();
             int x = 1;
-            Console.WriteLine("Default value of x: \t{0}", x);
+            Console.WriteLine("Default value of \tx: \t{0}", x);
             obj.ByValue(x);
-            Console.WriteLine("After value method x: \t{0}",x); // The value of x did not change
+            Console.WriteLine("After value method \tx: \t{0}",x); // The value of x did not change
             obj.ByReference(ref x);
-            Console.WriteLine("After reference method x: \t{0}", x); // The value of x changed
+            Console.WriteLine("After reference method \tx: \t{0}", x); // The value of x changed
             obj.ByOutput(out x);
-            Console.WriteLine("After output method x: \t{0}", x); // The value of x changed
+            Console.WriteLine("After output method \tx: \t{0}", x); // The value of x changed
             // More than one value can be returned from a function via passing parameters by Output
             Console.ReadLine();
         }
